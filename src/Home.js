38f020f55ac0badf0afb9aebd72becbcb05a517f
Ben Johnson <monk.sound.design@gmail.com>
2023-06-06 14:50:56 -0400

    home component complete

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import { Route, Routes, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
-
+        <div>
+            <h1>Welcome to the App</h1>
+            <Link to="/pizza">
+                <button id="order-pizza">Order Pizza</button>
+            </Link>
+        </div>
+        
     )
 }
