09459188b62554318f3c28a4a606e570900fdb22
Ben Johnson <monk.sound.design@gmail.com>
2023-06-06 15:22:22 -0400

    pizza form and inital state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from 'react-router-dom'
+import Home from "./Home"
+import PizzaForm from "./PizzaForm"
 
 
 const App = () => {
+  const initialFormValues = {
+    name: "",
+    size: "",
+    pepperoni: false,
+    mushrooms: false,
+    spinach: false,
+    sausage: false,
+    special: ""
+  }
+
+  const initialFormErrors = {
+    name: "",
+    size: ""
+  }
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="pizza" element={<PizzaForm />} />
     </Routes>
   );
 };
--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -2,6 +2,56 @@ import React from 'react'
 
 export default function PizzaForm() {
     return(
-        
+        <form id="pizza-form">
+            <input
+                id="name-input"
+                name="nameInput"
+                type="text"
+                placeholder="Name"
+                value={form.value}
+            />
+            <select id="size-dropdown">
+                <option>---- Select a Size ----</option>
+                <option>10" -- $12.95</option>
+                <option>12" -- $15.95</option>
+                <option>14" -- $17.95</option>
+            </select>
+            <label>Pepperoni
+            <input 
+                name="pepperoni"
+                type="checkbox"
+                checked={form.value === true}
+            /> 
+            </label>
+            <label>Mushrooms
+            <input 
+                name="mushrooms"
+                type="checkbox"
+                checked={form.value === true}
+            /> 
+            </label>
+            <label>Spinach
+            <input 
+                name="spinach"
+                type="checkbox"
+                checked={form.value === true}
+            /> 
+            </label>
+            <label>Sausage
+            <input 
+                name="sausage"
+                type="checkbox"
+                checked={form.value === true}
+            /> 
+            </label>
+            <input
+                id="special-text"
+                name="specialText"
+                type="text"
+                placeholder="Special Instructions"
+                value={form.value}
+            />
+            <button id="order-button">Add to Order</button>
+        </form>
     )
 }
