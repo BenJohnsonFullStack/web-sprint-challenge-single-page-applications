30b311a30dd6106f0df51f2d99f4d9781e570285
Ben Johnson <monk.sound.design@gmail.com>
2023-06-06 15:27:18 -0400

    postOrder helper

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Routes, Route } from 'react-router-dom'
+import axios from 'axios'
 import Home from "./Home"
 import PizzaForm from "./PizzaForm"
 
@@ -20,9 +21,25 @@ const App = () => {
     size: ""
   }
 
+  const initialOrders = []
+
   const [formValues, setFormValues] = useState(initialFormValues)
   const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [orders, setOrders] = useState(initialOrders)
 
+  const postOrder = newOrder => {
+    axios.post("http://localhost:3000/pizza", newOrder)
+      .then((res) => {
+        setOrders(res.data)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  }
+  
 
   return (
     <Routes>
